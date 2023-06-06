3a767e90df1b1d0fbce004d5e5306e7c329f1eaa
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-06 22:44:41 +0630

    fix rubocop errors

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::MessagesController < ApplicationController
-    def random_greeting
-        message = Message.order('RANDOM()').first
-        render json: { greeting: message.content }
-    end
+  def random_greeting
+    message = Message.order('RANDOM()').first
+    render json: { greeting: message.content }
+  end
 end
